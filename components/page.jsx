ae37f6c9ecae3c8ac8bd8d8f36792f5e3c044483
nalong <jjo.nalong@gmail.com>
2021-04-11 16:37:14 +0900

    페이지 스크롤 이벤트 적용

--- components/page.jsx
+++ components/page.jsx
@@ -3,7 +3,7 @@ import styles from '@/styles/components/page.module.scss';
 export default function Page(props)
 {
   return (
-    <div className={styles.page}>
+    <div className={styles.page} style={{...props.style}}>
       {props.children}
     </div>
   );
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import styles from '@/styles/pages/main.module.scss';
 
@@ -7,33 +7,68 @@ import Page from '@/components/page';
 import Home from '@/components/home';
 import Work from '@/components/work';
 
+const LIMIT_DELTA = 30;
+
 export default function MainPage()
 {
+  const pages = [<Home />, <Work />]
+
+  const [state, setState] = useState({
+    moving: false,
+    activePage: 0
+  });
+
   const handleScrollEvent = (e) =>
   {
-    console.log(e)
-  }
-
-  useEffect(() =>
-  {
-    document.addEventListener('wheel', handleScrollEvent, false);
-
-    return () =>
+    if (state.moving)
     {
-      document.removeEventListener('wheel', handleScrollEvent);
+      return;
     }
-  });
+    
+    if (Math.abs(e.deltaY) > LIMIT_DELTA)
+    {
+      if (e.deltaY < 0)
+      {
+        if (state.activePage === 0)
+        {
+          return;
+        }
+        setState({
+          moving: true,
+          activePage: state.activePage - 1
+        });
+        setTimeout(() => setState({
+          moving: false,
+          activePage: state.activePage - 1
+        }), styles.movingMs);
+      }
+      else
+      {
+        if (state.activePage === pages.length - 1)
+        {
+          return;
+        }
+        setState({
+          moving: true,
+          activePage: state.activePage + 1
+        });
+        setTimeout(() => setState({
+          moving: false,
+          activePage: state.activePage + 1
+        }), styles.movingMs);
+      }
+    }
+  }
 
   return (
     <main>
       <Circle />
-      <div className={styles.pages}>
-        <Page>
-          <Home />
-        </Page>
-        <Page>
-          <Work />
-        </Page>
+      <div className={styles.pages} onWheel={handleScrollEvent}>
+        {pages.map((page, i) =>
+          <Page key={i} style={{ top: (i - state.activePage) * styles.height }}>
+            {page}
+          </Page>  
+        )}
       </div>
     </main>
   );
