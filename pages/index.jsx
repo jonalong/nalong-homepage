161e5b0e8d61e021de692983b543f90633d93109
Taegyun Sooran Kim <T.SooranKim@gmail.com>
2021-04-18 18:31:14 +0900

    메인페이지 라우팅 (#1)
    * npm install
    
    * #hash를 이용해서 navigation 처리

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,6 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+import { useRouter } from 'next/router';
 
 import styles from '@/styles/pages/main.module.scss';
 
@@ -13,52 +15,75 @@ const LIMIT_DELTA = 15;
 
 export default function MainPage()
 {
+  const router = useRouter();
+
+  const hashes = ['', '#works', '#thanks'];
   const pages = [<Home />, <Work />, <div>thank you for wathing my website!</div>]
 
-  const [state, setState] = useState({
-    moving: false,
-    activePage: 0
+  const [moving, setMoving] = useState(false);
+  const [activePage, setActivePage] = useState(0);
+
+  useEffect(() =>
+  {
+    const handleHashChanged = (url) =>
+    {
+      const matchedResult = url.match(/^\/(#\w+|)$/);
+      if (!matchedResult)
+      {
+        return;
+      }
+      
+      const activePageIndex = hashes.indexOf(matchedResult[1]);
+      if (activePageIndex < 0 || activePageIndex == activePage)
+      {
+        return;
+      }
+
+      setActivePage(activePageIndex);
+      setTimeout(() => setMoving(false), styles.movingMs);
+    };
+    handleHashChanged(router.asPath);
+
+    router.events.on('hashChangeStart', handleHashChanged);
+    return () =>
+    {
+      router.events.off('hashChangeStart', handleHashChanged);
+    };
   });
 
   const handleScrollEvent = (e) =>
   {
-    if (state.moving)
+    if (moving)
     {
       return;
     }
     
     if (Math.abs(e.deltaY) > LIMIT_DELTA)
     {
+      let nextPage = -1;
       if (e.deltaY < 0)
       {
-        if (state.activePage === 0)
+        // UP
+        if (activePage === 0)
         {
-          return;
+          return 0;
         }
-        setState({
-          moving: true,
-          activePage: state.activePage - 1
-        });
-        setTimeout(() => setState({
-          moving: false,
-          activePage: state.activePage - 1
-        }), styles.movingMs);
+        nextPage = activePage - 1;
       }
       else
       {
-        if (state.activePage === pages.length - 1)
+        // DOWN
+        if (activePage === pages.length - 1)
         {
           return;
         }
-        setState({
-          moving: true,
-          activePage: state.activePage + 1
-        });
-        setTimeout(() => setState({
-          moving: false,
-          activePage: state.activePage + 1
-        }), styles.movingMs);
+        nextPage = activePage + 1;
       }
+
+      setMoving(true);
+      const hash = hashes[nextPage];
+      const nextUri = hash;
+      router.replace(nextUri);
     }
   }
 
@@ -68,7 +93,7 @@ export default function MainPage()
       <Circle />
       <div className={styles.pages} onWheel={handleScrollEvent}>
         {pages.map((page, i) =>
-          <Page key={i} style={{ top: (i - state.activePage) * styles.height }}>
+          <Page key={i} style={{ top: (i - activePage) * styles.height }}>
             {page}
           </Page>  
         )}
