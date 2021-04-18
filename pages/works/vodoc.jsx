d9cdd8f1d5674381c10dbb617434c7d15e28c8cf
nalong <jjo.nalong@gmail.com>
2021-04-18 19:50:04 +0900

    private 기능 추가

--- pages/works/vodoc.jsx
+++ pages/works/vodoc.jsx
@@ -1,7 +1,29 @@
+import { useState } from 'react';
+
 import styles from '@/styles/pages/workspage.module.scss';
+
+import { password } from '@/.private.json';
+
+import Voidoc from '@/components/private/voidoc';
 
 export default function VoidocPage()
 {
+  const [show, setShow] = useState(false);
+
+  const handleSubmit = (e) =>
+  {
+    e.preventDefault();
+
+    if (e.target.password.value === password)
+    {
+      setShow(true);
+    }
+    else
+    {
+      alert('틀렸습니다.');
+    }
+  };
+
   return (
     <div className={styles['work-page']}>
       <h1>
@@ -9,7 +31,11 @@ export default function VoidocPage()
         please enter password
       </h1>
       <div className={styles['work-text']}>원격진료 시스템 VOIDOC의 브랜딩과 사용자 어플리케이션, 관리자, 의사 웹 디자인</div>
-      <input type="password" placeholder="enter password" />
+      <form method="POST" onSubmit={handleSubmit}>
+        <input name="password" type="password" placeholder="enter password" />
+      </form>
+
+      {show && <Voidoc />}
     </div>
   )
 }
