ee981fdea5f53933bf7b26caf5ceb88a2523cd8b
nalong <jjo.nalong@gmail.com>
2021-04-25 04:12:17 +0900

    tosoone text 수정 및 링크 업로드

--- pages/works/tosoone.jsx
+++ pages/works/tosoone.jsx
@@ -5,9 +5,20 @@ export default function TosoonePage()
   return (
     <div className={styles['work-block']}>
       <p>
-      In the case of existing fairy tale books and language education learning systems, education is being conducted through courses such as reading books and following them. In this project, we tried to create educational teaching tools using interaction that allow children to learn and experience languages easily and quickly.<br/>
+        In the case of existing fairy tale books and language education learning systems, education is being conducted through courses such as reading books and following them. In this project, we tried to create educational teaching tools using interaction that allow children to learn and experience languages easily and quickly.
+        <br/><br/>
+        <a href="https://www.youtube.com/watch?v=MFxd776FGzg&ab_channel=%EC%A1%B0%EB%82%98%EB%A1%B1" target="blank">click here&nbsp;</a>to watch a video of my project.
       </p>
       <img src="/tosoone/main.jpg" alt="project image"/>
+      <img src="/tosoone/process.png" alt="project image"/>
+      <p>
+        Please insert your card into your device. 
+      </p>
+      <img src="/tosoone/tosoon1.png" alt="project image"/>
+      <img src="/tosoone/tosoon2.png" alt="project image"/>
+      <img src="/tosoone/01.png" alt="project image"/>
+      <img src="/tosoone/02.png" alt="project image"/>
+      <img src="/tosoone/03.png" alt="project image"/>
     </div>
   )
 }
